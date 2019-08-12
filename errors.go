14136dd5630c28fb2d9e5d1918ed800892d44e40
Tim French <tfrench15@gmail.com>
2019-08-11 22:42:00 -0700

    implement error interface for PersonasError

--- errors.go
+++ errors.go
@@ -2,11 +2,16 @@ package profile
 
 // PersonasError is returned when the Profile API returns an error.
 type PersonasError struct {
-	Error *ErrorCode
+	Err *ErrorCode
 }
 
 // ErrorCode details the reason the error occurred.
 type ErrorCode struct {
 	Code    string `json:"code"`
 	Message string `json:"message"`
+}
+
+// Error implements the error interface for PersonasError.
+func (e *PersonasError) Error() string {
+	return "CODE:\t" + e.Err.Code + "\nMESSAGE:\t" + e.Err.Message
 }
