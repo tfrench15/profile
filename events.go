d6d237cdfdf312d17bdddc6301fbffdf158720d9
Tim French <tfrench15@gmail.com>
2019-08-13 18:18:40 -0400

    create Event type, update GetEvents return

--- events.go
+++ events.go
@@ -1,33 +1,55 @@
 package profile
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 )
 
+// Event represents an event associated with a user's Profile.
+type Event map[string]interface{}
+
+// Events contain a slice of Event.
+type Events struct {
+	Data   Event   `json:"data"`
+	Cursor *Cursor `json:"cursor"`
+}
+
 // GetEvents queries the Profile API for the given ID's events.
-func (c *Client) GetEvents(id, value string) error {
+func (c *Client) GetEvents(id, value string) (*Events, error) {
 	url := baseURL + c.namespaceID + usersCollection + id + ":" + value + "/events"
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	req.SetBasicAuth(c.secret, "")
 
 	res, err := c.client.Do(req)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer res.Body.Close()
 
-	b, err := ioutil.ReadAll(res.Body)
+	events := newEvents()
+	dec := json.NewDecoder(res.Body)
+
+	err = dec.Decode(events)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	fmt.Println(string(b))
-	return nil
+	fmt.Println(events)
+	return events, nil
+}
+
+func newEvents() *Events {
+	m := make(Event)
+	c := new(Cursor)
+
+	return &Events{
+		Data:   m,
+		Cursor: c,
+	}
 }
