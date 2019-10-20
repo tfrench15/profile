673a880fa3d4c295b71acd7fa5eac98005ec922e
Tim French <tfrench15@gmail.com>
2019-10-19 17:33:36 -0700

    move some some funcs

--- types.go
+++ types.go
@@ -6,3 +6,21 @@ type Cursor struct {
 	HasMore bool   `json:"has_more"`
 	Next    string `json:"next"`
 }
+
+// Request represents a request that will be made to the Profile API.
+type Request interface {
+	// Validate ensures the request is valid.
+	Validate() error
+
+	// internal ensures no other package can create a type that is a valid request.
+	internal()
+}
+
+// Response represents possible responses from calling Query() on a Request.
+type Response interface {
+	// Marshal marshals the underlying data type into JSON.
+	Marshal() ([]byte, error)
+
+	// internal ensures no other package can create a type that is a valid request.
+	internal()
+}
